703ef2bc850034bd255ba6ce261d77589d1ae52b
Jeff Goldberg <jeffreybgoldberg@outlook.com>
2016-04-01 12:23:52 -0700

    Added files via upload

---
+++ lightMeter.ino
@@ -0,0 +1,35 @@
+// demo of Grove - Light Sensor
+#include <Grove_LED_Bar.h>
+
+Grove_LED_Bar bar(9, 8, 0);
+
+const int pinLight = A0;
+
+void setup()
+{
+      bar.begin();
+     Serial.begin(9600);
+    
+     
+}
+
+void loop()
+{
+    int value = analogRead(pinLight);
+    int show = (value/75);
+    if (show < 3) { show = 2 ;}
+    if (value < 80) { show = 1 ;}
+    
+   
+    
+    Serial.println(show);
+    
+    
+    bar.setLevel(show);
+ 
+  
+    
+}
+
+
+
